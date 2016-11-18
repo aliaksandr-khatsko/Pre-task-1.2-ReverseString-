4b772e3f9ad1c47848efa907d06e89c59dba727c
MINSK\Aliaksandr_Khatsko <aliaksandr.kh@gmail.com>
2016-11-18 19:18:23 +0300

    First commit pre-task 1.2 Reverse String

---
+++ StingReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StingReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter you sentence");
+            string stringLine = Console.ReadLine();
+            char[] stringArray = stringLine.ToCharArray();
+            Array.Reverse(stringArray);
+            Console.Write("You sentence is reversed: ");
+            Console.WriteLine(stringArray);
+            Console.ReadLine();
+        }
+    };
+}
